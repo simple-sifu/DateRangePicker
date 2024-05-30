967d4203408f3034013f27884b3e7249dfb21b1e
Tommy Han <tommy.han@ownbackup.com>
2024-05-30 01:47:24 -0400

    find red textbox

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,32 @@ const App = () => {
     console.log('Selected Date Range:', value);
   };
 
+  // const handleDateChange = (newDateRange: Date[] | null) => {
+  //   let beginDate = null;
+  //   let endDate = null;
+  //   if (newDateRange && newDateRange[0]) {
+  //     beginDate = new Date(newDateRange[0].setHours(0, 0, 0, 0));
+  //     endDate = new Date(newDateRange[1].setHours(23, 59, 59, 999));
+  //   }
+  //   console.log('beginDate =', beginDate, ", endDate =",endDate)
+  //   setDateRange(() => (beginDate ? [beginDate, endDate] : [null, null]));
+  // };
+
+  const currentMonth = new Date('2024-01-05T17:41:53');
+  const prevMonth = new Date('2023-07-14T17:10:25');
+
   // Function to disable dates
-  const shouldDisableDate = (date) => {
-    const today = new Date();
-    const isAfterToday = date > today;
-    return isAfterToday;
-  };
+  // const shouldDisableDate = (date: Date) => {
+  //  if (date < prevMonth || date > currentMonth){
+  //   return true;
+  //  }
+    
+  // };
+
+    const shouldDisableDate = (date: Date) => date < prevMonth || date > currentMonth;
 
   // Calculate the previous month and current month
-  const today = new Date();
-  const currentMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-  const prevMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+
 
   useEffect(() => {
     setCount((count) => count++);
@@ -35,11 +50,15 @@ const App = () => {
   return (
     <div>
       <DateRangePicker 
+        placeholder="Select Date Range"
         onChange={handleDateChange} 
         shouldDisableDate={shouldDisableDate} 
+        style={{ width: 255, fontFamily: 'Helvetica, Arial, sans-serif' }}
         defaultCalendarValue={[prevMonth, currentMonth]}
-        format="MM/dd/yyyy" 
-        character=" – "
+        ranges={[]}
+        format="MMM dd, yyyy"
+        character=" - "
+        showHeader={false}
       />
       <div>
         <p>Start Date: {dateRange && dateRange[0] ? dateRange[0].toString() : 'Not selected'}</p>
